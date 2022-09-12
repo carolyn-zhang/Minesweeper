81956a1f878640ed97886422dd6d2ccff3f1b62f
carolyn-zhang <cyzhang2001@gmail.com>
2022-09-11 19:40:07 -0700

    added random generator for 4 mines and functionality for revealing all mines when user picks one

--- app/src/main/java/com/example/gridlayout/MainActivity.java
+++ app/src/main/java/com/example/gridlayout/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int COLUMN_COUNT = 8;
     private int clock = 0;
-    private int numFlagsLeft = 10;
+    private int numFlagsLeft = 4; // 4 b/c we have 4 mines
     private boolean running = false;
     private boolean flag = false;
     private boolean pick = true;
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     // save the TextViews of all cells in an array, so later on,
     // when a TextView is clicked, we know which cell it is
     private ArrayList<TextView> cell_tvs;
+    private ArrayList<Integer> mineLocations;
 
     private int dpToPixel(int dp) {
         float density = Resources.getSystem().getDisplayMetrics().density;
@@ -37,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         cell_tvs = new ArrayList<TextView>();
         runTimer();
+        mineLocations = generateMineLocations();
 
         // Method (2): add four dynamically created cells
         GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
@@ -45,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 TextView tv = new TextView(this);
                 tv.setHeight( dpToPixel(32) );
                 tv.setWidth( dpToPixel(32) );
-                tv.setTextSize( 32 );//dpToPixel(32) );
+                tv.setTextSize( 24 );//dpToPixel(32) );
                 tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
                 tv.setTextColor(Color.GREEN);
                 tv.setBackgroundColor(Color.GREEN);
@@ -73,6 +75,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private ArrayList<Integer> generateMineLocations() {
+        ArrayList<Integer> mineLocations = new ArrayList<Integer>(4);
+        int randomNum;
+        while(mineLocations.size() < 4) {
+            randomNum = (int) (Math.random() * 80);
+            if(!mineLocations.contains(randomNum)){
+                mineLocations.add(randomNum);
+            }
+        }
+        for(int i = 0; i < mineLocations.size(); i++){
+            System.out.println(mineLocations.get(i));
+        }
+        return mineLocations;
+    }
+
     private int findIndexOfCellTextView(TextView tv) {
         for (int n=0; n<cell_tvs.size(); n++) {
             if (cell_tvs.get(n) == tv)
@@ -83,14 +100,16 @@ public class MainActivity extends AppCompatActivity {
 
     // user wants to toggle to setting flags now
     public void onClickPick(View view){
-        pick = false;
-        flag = true;
+        if(numFlagsLeft > 0) {
+            pick = false;
+            flag = true;
 
-        TextView pick = (TextView) findViewById(R.id.pick);
-        pick.setVisibility(TextView.INVISIBLE);
+            TextView pick = (TextView) findViewById(R.id.pick);
+            pick.setVisibility(TextView.INVISIBLE);
 
-        TextView flag = (TextView) findViewById(R.id.flag);
-        flag.setVisibility(TextView.VISIBLE);
+            TextView flag = (TextView) findViewById(R.id.flag);
+            flag.setVisibility(TextView.VISIBLE);
+        }
     }
 
     // user wants to toggle to picking now
@@ -105,6 +124,29 @@ public class MainActivity extends AppCompatActivity {
         pick.setVisibility(TextView.VISIBLE);
     }
 
+    // toggle to picking, call if no more flags left
+    public void toggleToPicking() {
+        flag = false;
+        pick = true;
+
+        TextView flag = (TextView) findViewById(R.id.flag);
+        flag.setVisibility(TextView.INVISIBLE);
+
+        TextView pick = (TextView) findViewById(R.id.pick);
+        pick.setVisibility(TextView.VISIBLE);
+    }
+
+    public void hidePickAndFlag() {
+        flag = false;
+        pick = false;
+
+        TextView flag = (TextView) findViewById(R.id.flag);
+        flag.setVisibility(TextView.INVISIBLE);
+
+        TextView pick = (TextView) findViewById(R.id.pick);
+        pick.setVisibility(TextView.INVISIBLE);
+    }
+
     public void onClickTV(View view){
         running = true;
         TextView tv = (TextView) view;
@@ -113,17 +155,34 @@ public class MainActivity extends AppCompatActivity {
         int j = n%COLUMN_COUNT;
         if(pick && tv.getText() == "") {
             // only pick cell if not already flagged
-            tv.setText(String.valueOf(i)+String.valueOf(j)); // cell value
-            if (tv.getCurrentTextColor() == Color.GREEN) {
-                tv.setTextColor(Color.GRAY);
-                tv.setBackgroundColor(Color.LTGRAY);
+            if(mineLocations.contains(n)){ // the cell picked has a mine
+                hidePickAndFlag(); // game over so don't let user pick/flag
+                revealMines();
+            } else { // the cell picked doesn't have a mine
+                if (tv.getCurrentTextColor() == Color.GREEN) {
+                    tv.setBackgroundColor(Color.LTGRAY);
+                    tv.setTextColor(Color.GRAY);
+                    tv.setText(String.valueOf(i)+String.valueOf(j)); // cell value
+                }
             }
-        } else if (flag && tv.getText() == "" && numFlagsLeft > 0) {
+        } else if (flag && tv.getText() == "") {
             // only flag cell if not already picked
             tv.setText(R.string.flag);
             final TextView flagsLeft = (TextView) findViewById(R.id.flagsLeftValue);
             numFlagsLeft -= 1;
             flagsLeft.setText(String.valueOf(numFlagsLeft));
+            if(numFlagsLeft == 0) { // no more flags left, go to pick
+                toggleToPicking();
+            }
+        }
+    }
+
+    private void revealMines() {
+        for(int i = 0; i < cell_tvs.size(); i++) {
+            if(mineLocations.contains(i)) { // cell_tvs[i] is a mine
+                cell_tvs.get(i).setBackgroundColor(Color.LTGRAY);
+                cell_tvs.get(i).setText(R.string.mine);
+            }
         }
     }
 
